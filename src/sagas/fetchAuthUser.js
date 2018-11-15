3083e0e8028353facef7adeb1855dcfe362af226
Jiří Čermák <jiri.cermak@ackee.cz>
2018-11-15 16:29:15 +0100

    Update fetchAuthUser - prevent multiple duplicit requests

--- src/sagas/fetchAuthUser.js
+++ src/sagas/fetchAuthUser.js
@@ -1,17 +1,12 @@
-import { takeEvery, put, select } from 'redux-saga/effects';
+import { take, put } from 'redux-saga/effects';
 
 import { FETCH_AUTH_USER_REQUEST } from '../actionType';
 import { fetchAuthUserFailure, fetchAuthUserSuccess } from '../actions';
-import { isUserFetching } from '../selectors';
 
 import config from './config';
 
 function* fetchAuthUser() {
     try {
-        const isFetching = yield select(isUserFetching);
-
-        if (isFetching) return;
-
         const user = yield config.remoteGetAuthUser();
 
         yield put(fetchAuthUserSuccess(user));
@@ -22,5 +17,8 @@ function* fetchAuthUser() {
 }
 
 export default function*() {
-    yield takeEvery(FETCH_AUTH_USER_REQUEST, fetchAuthUser);
+    while (true) {
+        yield take(FETCH_AUTH_USER_REQUEST);
+        yield fetchAuthUser();
+    }
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,12 @@ import { all } from 'redux-saga/effects';
 import tokens, { tryToRetrieveTokens } from './tokens';
 import handleLogin from './handleLogin';
 import processTokenRefresh from './processTokenRefresh';
+import fetchAuthUser from './fetchAuthUser';
 
 export * from './authStateChannel';
 export { default as configure } from './configure';
 export { default as authorizedFn } from './authorizedFn';
 
 export function* saga() {
-    yield all([handleLogin(), processTokenRefresh(), tokens(), tryToRetrieveTokens()]);
+    yield all([handleLogin(), processTokenRefresh(), fetchAuthUser(), tokens(), tryToRetrieveTokens()]);
 }
