59911f158175e2dbb2576f7d57837c8fc5b6c284
Jiří Čermák <jiri.cermak@ackee.cz>
2018-11-15 16:27:28 +0100

    Update action types - add to each type package prefix

--- src/actionType.js
+++ src/actionType.js
@@ -1,16 +1,16 @@
-export const AUTH_LOGIN = 'AUTH_LOGIN';
-export const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS';
-export const AUTH_LOGIN_FAILURE = 'AUTH_LOGIN_FAILURE';
+export const AUTH_LOGIN = '@@redux-token-auth/AUTH_LOGIN';
+export const AUTH_LOGIN_SUCCESS = '@@redux-token-auth/AUTH_LOGIN_SUCCESS';
+export const AUTH_LOGIN_FAILURE = '@@redux-token-auth/AUTH_LOGIN_FAILURE';
 
-export const AUTH_LOGOUT = 'AUTH_LOGOUT';
+export const AUTH_LOGOUT = '@@redux-token-auth/AUTH_LOGOUT';
 
-export const SET_AUTH_TOKENS = 'SET_AUTH_TOKENS';
+export const SET_AUTH_TOKENS = '@@redux-token-auth/SET_AUTH_TOKENS';
 
-export const AUTH_REFRESH_TOKEN = 'AUTH_REFRESH_TOKEN';
-export const AUTH_REFRESH_TOKEN_SUCCESS = 'AUTH_REFRESH_TOKEN_SUCCESS';
-export const AUTH_REFRESH_TOKEN_FAILURE = 'AUTH_REFRESH_TOKEN_FAILURE';
-export const PROCESS_TOKEN_REFRESH = 'PROCESS_TOKEN_REFRESH';
+export const AUTH_REFRESH_TOKEN = '@@redux-token-auth/AUTH_REFRESH_TOKEN';
+export const AUTH_REFRESH_TOKEN_SUCCESS = '@@redux-token-auth/AUTH_REFRESH_TOKEN_SUCCESS';
+export const AUTH_REFRESH_TOKEN_FAILURE = '@@redux-token-auth/AUTH_REFRESH_TOKEN_FAILURE';
+export const PROCESS_TOKEN_REFRESH = '@@redux-token-auth/PROCESS_TOKEN_REFRESH';
 
-export const FETCH_AUTH_USER_REQUEST = 'FETCH_AUTH_USER_REQUEST';
-export const FETCH_AUTH_USER_SUCCESS = 'FETCH_AUTH_USER_SUCCESS';
-export const FETCH_AUTH_USER_FAILURE = 'FETCH_AUTH_USER_FAILURE';
+export const FETCH_AUTH_USER_REQUEST = '@@redux-token-auth/FETCH_AUTH_USER_REQUEST';
+export const FETCH_AUTH_USER_SUCCESS = '@@redux-token-auth/FETCH_AUTH_USER_SUCCESS';
+export const FETCH_AUTH_USER_FAILURE = '@@redux-token-auth/FETCH_AUTH_USER_FAILURE';
