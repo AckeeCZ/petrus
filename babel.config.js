cf57c62046db267e905f1175c1315c0e8e662aa1
Jiří Čermák <jiri.cermak@ackee.cz>
2018-11-27 23:08:44 +0100

    2.2.7

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function(api) {
+    const plugins = [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-transform-runtime',
+    ];
+
+    const presets = {
+        lib: ['@babel/preset-env', '@babel/preset-react'],
+        es: [
+            [
+                '@babel/preset-env',
+                {
+                    modules: false,
+                },
+            ],
+            '@babel/preset-react',
+        ],
+    };
+
+    return {
+        plugins,
+        presets: presets[api.env()],
+    };
+};
