8c4b99d7f4ac1a75bcaebe14ab6f0e75ac765c5d
Jiří Čermák <jiri.cermak@ackee.cz>
2019-01-25 10:24:47 +0100

    Fix toCapitalize

--- src/sagas/utilities/getSearchParams.js
+++ src/sagas/utilities/getSearchParams.js
@@ -1,5 +1,5 @@
 function toCapitalize(string) {
-    return string[0].toUpperCase() + string.slice(1);
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
 
 function fromSnakeToCamelCase(string) {
