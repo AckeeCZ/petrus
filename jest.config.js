7fddcd9e01b55aa2e5883aa7ab3a102283cc99ae
Jiří Čermák <jiri.cermak@ackee.cz>
2019-04-29 10:21:56 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 const { jestAliases } = require('./config/aliases');
 
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules', '/lib', '/es'],
-    setupTestFrameworkScriptFile: './configure-jest.js',
+    testPathIgnorePatterns: ['node_modules', 'lib', 'es'],
+    setupFilesAfterEnv: ['./configure-jest.js'],
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$',
     moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
     moduleNameMapper: jestAliases,
